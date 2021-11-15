371a4553c5e7c266ce448211ff96a96fa555f40d
liulong@psylife.cc <dfsd534@qq.com>
2021-11-15 20:11:03 +0800

    go web

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dfsd534/go_web
 
 go 1.15
+
+require github.com/lib/pq v1.10.4
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"github.com/dfsd534/go_web/data"
+	"net/http"
+)
 
 func main() {
 	mux := http.NewServeMux()
@@ -15,4 +18,18 @@ func main() {
 	}
 
 	server.ListenAndServe()
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if threads, err := data.Threads(); err == nil {
+		_, err := session(w, r)
+		public_tmpl_files := []string{"layout", "public.navbar", "index"}
+		private_tmpl_files := []string{"layout", "private.navbar", "index"}
+
+		if err != nil {
+			generateHTML(w, threads, public_tmpl_files...)
+		} else {
+			generateHTML(w, threads, private_tmpl_files...)
+		}
+	}
 }
